844481d8d2ed980f55abf5df4e3a48a48edfc6db
Brenton Major <brenton.major@loves.com>
2018-07-10 17:18:41 -0500

    Added Album and Track Interface

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 
 import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -10,7 +13,7 @@ export class ProductService {
 
   private _albumUrl = '../assets/album.json';
 
-  getAlbum(id: number){
-    return this._http.get(this._albumUrl).map((response) => response.json());
+  getAlbum(id: number): Observable<Album>{
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 }
